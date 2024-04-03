434e6e1996d054243c504a657acf2754e8ea30db
Yash Gupta <yash.gupta02@nagarro.com>
2024-04-03 14:19:38 +0530

    added test fix

--- src/test/java/com/nagarro/springbootdocker/SpringbootDockerApplicationTests.java
+++ src/test/java/com/nagarro/springbootdocker/SpringbootDockerApplicationTests.java
@@ -1,6 +1,7 @@
 package com.nagarro.springbootdocker;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
